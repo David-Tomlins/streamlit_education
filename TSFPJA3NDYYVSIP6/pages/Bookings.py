0c3efed82717305e4e20e7704debf186ec54c7ff
David Tomlins <david.tomlins@ometis.co.uk>
2025-04-08 09:42:20 +0000

    corect pages file name

---
+++ TSFPJA3NDYYVSIP6/pages/Bookings.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Training Portal", page_icon="📚")
+
+st.title("Welcome to the Training Management System")
+st.markdown("Use the sidebar to navigate between pages.")
